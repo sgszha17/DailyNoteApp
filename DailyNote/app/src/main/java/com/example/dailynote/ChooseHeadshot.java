8424dd09bb53af6976eaa326040d25a04cd225a8
MrKelly94 <tongtracy94@yahoo.com>
2017-09-27 16:48:56 +1000

    changed files

---
+++ DailyNote/app/src/main/java/com/example/dailynote/ChooseHeadshot.java
@@ -0,0 +1,14 @@
+package com.example.dailynote;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class ChooseHeadshot extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_headshot);
+    }
+}
