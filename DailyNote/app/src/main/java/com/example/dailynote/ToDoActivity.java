221e594bea9471fbe7058579fa5212a1de23253a
brightsuns <376215031@qq.com>
2017-10-08 01:37:16 +1100

    Added Google Signin for Siyu

--- DailyNote/app/src/main/java/com/example/dailynote/ToDoActivity.java
+++ DailyNote/app/src/main/java/com/example/dailynote/ToDoActivity.java
@@ -18,6 +18,8 @@ import java.util.concurrent.TimeUnit;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.Fragment;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -28,6 +30,7 @@ import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.Preference;
+import android.support.v4.app.FragmentActivity;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.util.TypedValue;
@@ -41,6 +44,14 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.OptionalPendingResult;
+import com.google.android.gms.common.api.ResultCallback;
 import com.google.common.util.concurrent.FutureCallback;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
@@ -61,7 +72,11 @@ import com.squareup.okhttp.OkHttpClient;
 
 import static android.content.ContentValues.TAG;
 
-public class ToDoActivity extends Activity {
+public class ToDoActivity extends FragmentActivity implements
+        GoogleApiClient.OnConnectionFailedListener,
+        View.OnClickListener{
+    private static final int RC_SIGN_IN = 9001;
+    private ProgressDialog mProgressDialog;
     private LinearLayout mainLayout;
     private TextView appName;
     private AssetManager assetManager;
@@ -72,6 +87,8 @@ public class ToDoActivity extends Activity {
     private ImageButton wechat_login;
 
     private List<Note> data;
+
+    private GoogleApiClient mGoogleApiClient;
 
     private RegisterDialog registerDialog;
     private String mUsername="";
@@ -132,20 +149,33 @@ public class ToDoActivity extends Activity {
 
             }
         });
+
+
+        /* google play initialize*/
+        // [START configure_signin]
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        // [END configure_signin]
+
+        // [START build_client]
+        // Build a GoogleApiClient with access to the Google Sign-In API and the
+        // options specified by gso.
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+        // [END build_client]
         google_login = (ImageButton) findViewById(R.id.google);
-        google_login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        google_login.setOnClickListener(this);
+        /* google play initialize*/
 
-            }
-        });
+        
+
         wechat_login = (ImageButton) findViewById(R.id.wechat);
-        wechat_login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
+        wechat_login.setOnClickListener(this);
 
         progressBar = (ProgressBar) findViewById(R.id.loadingProgressBar);
 
@@ -180,6 +210,120 @@ public class ToDoActivity extends Activity {
         loginAndRegister();
     }
 
+    //****google sign in start****
+    //silent google sign in commented for siyu
+
+    /**
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        OptionalPendingResult<GoogleSignInResult> opr = Auth.GoogleSignInApi.silentSignIn(mGoogleApiClient);
+        if (opr.isDone()) {
+            // If the user's cached credentials are valid, the OptionalPendingResult will be "done"
+            // and the GoogleSignInResult will be available instantly.
+            Log.d(TAG, "Got cached sign-in");
+            GoogleSignInResult result = opr.get();
+            handleSignInResult(result);
+        } else {
+            // If the user has not previously signed in on this device or the sign-in has expired,
+            // this asynchronous branch will attempt to sign in the user silently.  Cross-device
+            // single sign-on will occur in this branch.
+            showProgressDialog();
+            opr.setResultCallback(new ResultCallback<GoogleSignInResult>() {
+                @Override
+                public void onResult(GoogleSignInResult googleSignInResult) {
+                    hideProgressDialog();
+                    handleSignInResult(googleSignInResult);
+                }
+            });
+        }
+    }*/
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        hideProgressDialog();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mProgressDialog != null) {
+            mProgressDialog.dismiss();
+        }
+    }
+
+    // [START onActivityResult]
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+    // [END onActivityResult]
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.google:
+                signIn();
+                break;
+
+        }
+    }
+
+    private void showProgressDialog() {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setMessage("Loading");
+            mProgressDialog.setIndeterminate(true);
+        }
+
+        mProgressDialog.show();
+    }
+
+    private void hideProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.hide();
+        }
+    }
+
+    // [START handleSignInResult]
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d(TAG, "handleSignInResult:" + result.isSuccess());
+        if (result.isSuccess()) {
+            // Signed in successfully, show authenticated UI.
+            GoogleSignInAccount acct = result.getSignInAccount();
+            loginSucceed();
+        } else {
+            // Signed out, show unauthenticated UI.
+            Toast.makeText(this, "Google Signin failed. Please try again.",
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+
+    // [START signIn]
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+    // [END signIn]
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        // An unresolvable error has occurred and Google APIs (including Sign-In) will not
+        // be available.
+        Log.d(TAG, "onConnectionFailed:" + connectionResult);
+        Toast.makeText(this, "Connection Failed",
+                Toast.LENGTH_LONG).show();
+    }
+    //****Google sign in End****
 
 
 
